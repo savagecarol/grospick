8a0cb0e982d7ec21a0975ea8449da934c36ee9f2
savagecarol <karthiksharma1411@gmail.com>
2020-09-27 14:23:31 +0530

    hello

--- lib/services/firebase_auth_service.dart
+++ lib/services/firebase_auth_service.dart
@@ -4,10 +4,10 @@ import 'dart:core';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthService {
-    FirebaseAuthService._();
+  FirebaseAuthService._();
   factory FirebaseAuthService.getInstance() => _instance;
   static final FirebaseAuthService _instance = FirebaseAuthService._();
-  
+
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   Future<String> signIn(String email, String password) async {
@@ -15,7 +15,10 @@ class FirebaseAuthService {
       AuthResult authResult = await _firebaseAuth.signInWithEmailAndPassword(
           email: email, password: password);
       FirebaseUser user = authResult.user;
-      return user.uid;
+      if (user.isEmailVerified) {
+        return user.uid;
+      }
+      return null;
     } catch (e) {
       print("error in signout");
       print(e);
@@ -24,12 +27,16 @@ class FirebaseAuthService {
 
   Future<String> signUp(String email, String password) async {
     try {
-
-      
       AuthResult authResult = await _firebaseAuth
           .createUserWithEmailAndPassword(email: email, password: password);
       FirebaseUser user = authResult.user;
-      return user.uid;
+      try {
+        await user.sendEmailVerification();
+        return user.uid;
+      } catch (e) {
+        print("An error occured while trying to send email verification");
+        print(e.message);
+      }
     } catch (e) {
       print("error in signout");
       print(e);
@@ -38,13 +45,10 @@ class FirebaseAuthService {
   }
 
   Future<String> getCurrentuser() async {
-    
-    
     FirebaseUser user = await _firebaseAuth.currentUser();
     return user.uid;
-
-
   }
+
   Future<bool> signOut() async {
     try {
       await _firebaseAuth.signOut();
